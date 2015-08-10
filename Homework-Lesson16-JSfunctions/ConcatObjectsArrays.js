ce6ba90ec653ba69b17ae5ac9ede53c1df696622
JulyV <1_july@list.ru>
2015-08-10 19:40:54 +0300

    Update ConcatObjectsArrays.js

--- Homework-Lesson16-JSfunctions/ConcatObjectsArrays.js
+++ Homework-Lesson16-JSfunctions/ConcatObjectsArrays.js
@@ -1,5 +1,6 @@
 function ConcatObjects () {
 	var i = 0;
+	var result = 0;
 	
 	for  ( i = 0; i < arguments.length; i++ ) {
 		if( Array.isArray(arguments[i]) === true ) {
@@ -7,17 +8,24 @@ function ConcatObjects () {
 				for  ( i = 0; i < arguments.length; i++) {
 				result = result.concat(arguments[i]);
 				}
-			} else {
+			} else if (typeof arguments[i] === "object") {
 				result = {};
 				for  ( i = 0; i < arguments.length; i++) {
 					for (var attrname in arguments[i]) { 
 					result[attrname] = arguments[i][attrname]; 
 					}
 				}	
-			} 
+				
+			} else {
+				result += arguments[i];
+			}
 	}
 	return result;
 	
 }
 
+ConcatObjects (1,2,3)  
+
 ConcatObjects ({a: 1, b: 2}, {c: 3, d: 4})  
+
+ConcatObjects ([1,2,3], [4,5])
